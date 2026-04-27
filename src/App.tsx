d9850285312f012d984ae95a16b33550698be7d5
Ninja 77030 <n77030@outlook.com>
2026-04-27 09:14:01 +0530

    Removed the default template to add the socket.io interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useState, useRef } from 'react';
+import client from './services/client'
 import './App.css';
 
 function App() {
+  const [errorMessage, setError] = useState<string | null>(null)
+  useRef(() => {
+    client.connect()
+    client.on('error', (err: any) => {
+        setError(err.message)
+    })
+  })
+  const sendDirection = (direction: string) => {
+      client.emit('direction', direction)
+      console.log(direction)
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Control your cybernetic arm</h1>
+        <p style={{color: "red"}}>{errorMessage}</p>
       </header>
+      <div className="control-panel">
+          <button onClick={() => sendDirection('w')}>Up</button>
+          <button onClick={() => sendDirection('a')}>Left</button>
+          <button onClick={() => sendDirection('s')}>Down</button>
+          <button onClick={() => sendDirection('d')}>Right</button>
+      </div>
     </div>
   );
 }
