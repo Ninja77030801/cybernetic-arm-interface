13fdcd95291bfce5ca2d00c7b7ddac0a304c2a1b
Ninja 77030 <n77030@outlook.com>
2026-06-19 23:03:51 +0530

    Changed the port of the backend

--- src/services/client.ts
+++ src/services/client.ts
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-var client: any = io('http://127.0.0.1:5000', {transports: ['websocket']});
+var client: any = io('http://127.0.0.1:4000', {transports: ['websocket']});
 
 export default client;
