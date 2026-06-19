d22cdfb5621838e67ab13393c293a1de94c29405
Ninja 77030 <n77030@outlook.com>
2026-06-19 23:06:11 +0530

    Ignoring the android directory since I'm pretty sure it's not required

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ninja77030.cyberneticarm',
+  appName: 'cybernetic-arm',
+  webDir: 'dist-react'
+};
+
+export default config;
